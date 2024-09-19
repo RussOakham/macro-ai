f43bee9fb1a33de91f98b66c644a684c03280761
Russell Oakham <russell.oakham@and.digital>
2024-09-19 10:41:35 +0100

    [feat] api - delete tsoa based api@

--- apps/api/dist/src/server/server.js
+++
@@ -1,8 +0,0 @@
-var _a;
-import { app } from "../app";
-import { logger as pino } from "../services/logger";
-const { logger } = pino;
-const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : "3030";
-app.listen(port, () => {
-    logger.info(`Example app listening at http://localhost:${port}`);
-});
--- apps/api/src/server/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "../app";
-import { logger as pino } from "../services/logger";
-
-const { logger } = pino;
-const port = process.env.PORT ?? "3030";
-
-app.listen(port, () => {
-  logger.info(`Example app listening at http://localhost:${port}`);
-});
