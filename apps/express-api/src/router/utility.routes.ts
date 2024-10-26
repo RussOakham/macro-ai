3d7cf2220685b2f7284acacf641d202edd62813a
Russell Oakham <rjoakham@gmail.com>
2024-10-26 21:13:03 +0100

    feat(express-api): swagger added and health endpoint documented

--- apps/express-api/src/router/utility.routes.ts
+++ apps/express-api/src/router/utility.routes.ts
@@ -1,8 +1,61 @@
 import { type Router } from 'express'
 
+import { pino } from '../utils/logger.ts'
+
+const { logger } = pino
+
 const utilityRouter = (router: Router) => {
+	/**
+	 * @swagger
+	 * tags:
+	 *   name: Utility
+	 *   description: Utility endpoints
+	 * /health:
+	 *   get:
+	 *     tags:
+	 *       - Utility
+	 *     summary: Check if the server is running
+	 *     responses:
+	 *       '200':
+	 *         description: Server is running
+	 *         content:
+	 *           application/json:
+	 *             schema:
+	 *               $ref: '#/components/schemas/HealthResponse'
+	 *       '500':
+	 *         description: Server is not running
+	 *         content:
+	 *           application/json:
+	 *             schema:
+	 *               $ref: '#/components/schemas/ErrorResponse'
+	 * components:
+	 *   schemas:
+	 *     HealthResponse:
+	 *       type: object
+	 *       properties:
+	 *         message:
+	 *           type: string
+	 *           description: The health status of the api
+	 *       example:
+	 *         message: "Api Health Status: OK"
+	 *     ErrorResponse:
+	 *       type: object
+	 *       properties:
+	 *         message:
+	 *           type: string
+	 *           description: The error message
+	 *       example:
+	 *         message: "Api Status: Error"
+	 */
 	router.get('/health', (req, res) => {
-		res.send('OK')
+		try {
+			res.status(200).json({ message: 'Api Health Status: OK' })
+		} catch (error: unknown) {
+			logger.error(
+				`[utilityRouter]: Error checking health status: ${(error as Error).message}`,
+			)
+			res.status(500).json({ message: 'Api Status: Error' })
+		}
 	})
 }
 
--- apps/express-api/src/utils/server.ts
+++ apps/express-api/src/utils/server.ts
@@ -2,9 +2,42 @@ import express, { Express } from 'express'
 import compression from 'compression'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import swaggerJSDoc, { type Options } from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 import { pino } from './logger.ts'
 import { appRouter } from '../router/index.routes.ts'
+
+const options: Options = {
+	definition: {
+		openapi: '3.1.0',
+		info: {
+			title: 'Macro AI Express API with Swagger',
+			version: '0.0.0',
+			description:
+				'This is a simple CRUD API application made with Express and documented with Swagger',
+			license: {
+				name: 'MIT',
+				url: 'https://spdx.org/licenses/MIT.html',
+			},
+		},
+		servers: [
+			{
+				url: 'http://localhost:3030/api',
+			},
+		],
+	},
+	apis: ['./src/router/*.routes.ts'],
+	components: {
+		schemas: {
+			HealthResponse: {
+				type: 'object',
+			},
+		},
+	},
+}
+
+const specs = swaggerJSDoc(options)
 
 const createServer = (): Express => {
 	const app: Express = express()
@@ -19,7 +52,11 @@ const createServer = (): Express => {
 	app.use(bodyParser.json())
 
 	app.use('/api', appRouter())
-
+	app.use(
+		'/api-docs',
+		swaggerUi.serve,
+		swaggerUi.setup(specs, { explorer: true }),
+	)
 	return app
 }
 
