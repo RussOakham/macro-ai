86348cf35b9749b818299ef4c9b06554c4182d41
Russell Oakham <rjoakham@gmail.com>
2025-02-09 23:29:53 +0000

    docs(zustand auth): dev comments

--- apps/client-ui/src/components/providers/auth-provider.tsx
+++ apps/client-ui/src/components/providers/auth-provider.tsx
@@ -1,6 +1,13 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
+// TODO: Build out further
+// TODO: Add way to initialize store at app start
+// TODO: Rename file to auth-store.ts and create store file for each store
+// Ref: https://blog.stackademic.com/zustand-for-authentication-in-react-apps-156b6294129c
+// Ref: https://doichevkostia.dev/blog/authentication-store-with-zustand/
+// Ref: 
+
 interface IUser {
 	email: string
 	accessToken: string
