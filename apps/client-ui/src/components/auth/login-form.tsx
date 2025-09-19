ae8bfa7e94a7f650704cd4f43e18b2344e8ed2bc
Russell Oakham <rjoakham@gmail.com>
2025-09-19 22:40:19 +0100

    docs(note) - dev note on jsx

--- apps/client-ui/src/components/auth/login-form.tsx
+++ apps/client-ui/src/components/auth/login-form.tsx
@@ -115,7 +115,7 @@ const LoginForm = ({
 						</Button>
 					</form>
 				</Form>
-
+				{/* Re-add with better-auth */}
 				{/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
 					<span className="relative z-10 bg-background px-2 text-muted-foreground">
 						Or continue with
