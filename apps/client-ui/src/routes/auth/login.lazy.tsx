edf7410352438d646a77119a0319a7d72ac633a1
Russell Oakham <rjoakham@gmail.com>
2025-09-19 22:03:42 +0100

    fix(client-ui): update login page company title

--- apps/client-ui/src/routes/auth/login.lazy.tsx
+++ apps/client-ui/src/routes/auth/login.lazy.tsx
@@ -30,7 +30,7 @@ const RouteComponent = () => {
 						<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
 							<GalleryVerticalEnd className="size-4" />
 						</div>
-						Acme Inc.
+						Macro AI.
 					</Link>
 				</div>
 				<div className="flex flex-1 items-center justify-center">
